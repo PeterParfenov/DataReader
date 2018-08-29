3b6b85db40e999df2c450017910634f731d0acf5
PeterParfenov <terrylapard@gmail.com>
2018-08-29 13:32:47 +0300

    v3 added (with y-symetrization for pT)

--- DataReaderPlotter.cxx
+++ DataReaderPlotter.cxx
@@ -331,13 +331,13 @@ void DataReaderPlotter::Fill(DataReaderEvent *_event, Double_t _weight = 1.)
                 {
                   fHistogramFlow.at(iCentrality)[TString("hv" + std::to_string(iHarm + 1) + FlowCentralityName[iCentrality] + "Rapidity" + ParticleName[iPID])]->Fill(Rapidity, TMath::Cos((iHarm + 1) * (phi - _event->PsiRP)), _weight);
                   fHistogramFlow.at(iCentrality)[TString("hv" + std::to_string(iHarm + 1) + FlowCentralityName[iCentrality] + "Eta" + ParticleName[iPID])]->Fill(Eta, TMath::Cos((iHarm + 1) * (phi - _event->PsiRP)), _weight);
-                  if (iHarm != 0)
+                  if (iHarm != 0 || iHarm != 2)
                   {
                     fHistogramFlow.at(iCentrality)[TString("hv" + std::to_string(iHarm + 1) + FlowCentralityName[iCentrality] + "Pt" + ParticleName[iPID])]->Fill(Pt, TMath::Cos((iHarm + 1) * (phi - _event->PsiRP)), _weight);
 
                     fHistogramFlow.at(iCentrality)[TString("hv" + std::to_string(iHarm + 1) + FlowCentralityName[iCentrality] + "B" + ParticleName[iPID])]->Fill(_event->B, TMath::Cos((iHarm + 1) * (phi - _event->PsiRP)), _weight);
                   }
-                  if (iHarm == 0 && TMath::Abs(Rapidity) > FlowMidRapidityCutForPt)
+                  if ((iHarm == 0 || iHarm == 2) && TMath::Abs(Rapidity) > FlowMidRapidityCutForPt)
                   {
                     wV1 = TMath::Sign(1, Rapidity);
                     fHistogramFlow.at(iCentrality)[TString("hv" + std::to_string(iHarm + 1) + FlowCentralityName[iCentrality] + "Pt" + ParticleName[iPID])]->Fill(Pt, wV1 * TMath::Cos((iHarm + 1) * (phi - _event->PsiRP)), _weight);
